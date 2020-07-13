3a1b2a4786455f030b82fe13855007a5604fff03
unknown <you@example.com>
2020-07-13 20:22:53 -0300

    add delete button

--- simple-scrapbook-refactor_to_es6/main.js
+++ simple-scrapbook-refactor_to_es6/main.js
@@ -7,22 +7,31 @@ class TaskList {
 
     this.scraps = [];
 
-    this.registerEvents();
+    this.setAddButtonEvent();
   }
 
-  registerEvents() {
+  setAddButtonEvent() {
     this.addButton.onclick = () => this.addNewScrap();
+  }
+
+  setButtonEvents() {
+    document.querySelectorAll(".delete-button").forEach((item) => {
+      item.onclick = () => thisdeleteScrap;
+    });
   }
 
   renderScraps() {
     this.scrapsField.innerHTML = "";
 
     for (const scrap of this.scraps) {
+      let position = scraps.indexOf(scrap); /* */
       this.scrapsField.innerHTML += this.createScrapCard(
         scrap.title,
-        scrap.message
+        scrap.message,
+        position
       );
     }
+    this.setButtonEvents();
   }
 
   addNewScrap() {
@@ -37,6 +46,12 @@ class TaskList {
     this.renderScraps();
   }
 
+  deleteScrap(position) {
+    this.scraps.splice(position, 1);
+
+    this.renderScraps();
+  }
+
   createScrapCard(title, message) {
     return `
       <div class="message-cards card text-white bg-dark m-2 col-3">
@@ -47,7 +62,7 @@ class TaskList {
           </p>
         </div>
         <div class="w-100 d-flex justify-content-end pr-2 pb-2">
-          <button class="btn btn-danger mr-1">Deletar</button>
+          <button class="btn btn-danger mr-1 delete-button">Deletar</button>
           <button class="btn btn-info">Editar</button>
         </div>
       </div>
