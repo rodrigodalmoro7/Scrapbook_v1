86069f628e1c006341f29fcec73c98c3d703bb59
unknown <you@example.com>
2020-08-17 22:36:30 -0300

    Add :id on ScrapId for DELETE ROUTE

--- src/index.js
+++ src/index.js
@@ -119,7 +119,7 @@ class TaskList {
     let message = this.editMessageInput.value;
 
     await api.put(`/scraps/${scrapId}`, { title, message });
-    this.scraps[scrapIndex] = { scrapId, title, message };
+    this.scraps[scrapIndex] = { id: scrapId, title, message };
 
     this.renderScraps();
     $("#editModal").modal("hide");
